618b0de94a2ae996d357eb0d2f17ed8da5339c13
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2020-01-07 13:15:33 +0100

    [feature] - Added styles

---
+++ src/styles/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    margin: theme.spacing(1),
+  },
+  link: {
+    margin: theme.spacing(1),
+  },
+}));
